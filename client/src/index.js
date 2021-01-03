b6520629198c53b6fa3ca4afb862f65e28a95baa
prateekchandra <prateekchandra1027@gmail.com>
2021-01-03 15:54:59 +0530

    Added route for register

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import App from './App';
+import Register from './screens/Register.jsx';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path='/' exact render={props => <App {...props} />} />
+    
+    <Route path='/register' exact render={props => <Register {...props} />} />
+    
+    <Redirect to='/' />
+  </Switch>
+</BrowserRouter>,
   document.getElementById('root')
 );
 
---
+++ client/src/screens/Register.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import authSvg from '../assests/auth.svg';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import { authenticate, isAuth } from '../helpers/auth';
+import { Link, Redirect } from 'react-router-dom';
+
+
+
+function Register() {
+    return (
+        <div>
+           Hi 
+        </div>
+    )
+}
+
+export default Register
