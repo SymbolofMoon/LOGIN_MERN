817602a7027f8f2fe1e7c96f0da5783ea6f69b19
prateekchandra <prateekchandra1027@gmail.com>
2021-01-03 20:27:12 +0530

    Login functionality via frontend

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import App from './App';
 import Register from './screens/Register.jsx';
 import Activate from './screens/Activate.jsx';
+import Login from './screens/Login.jsx';
 import 'react-toastify/dist/ReactToastify.css';
-
+import ForgetPassword from './screens/ForgetPassword.jsx';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,8 +14,9 @@ ReactDOM.render(
     <Route path='/' exact render={props => <App {...props} />} />
     
     <Route path='/register' exact render={props => <Register {...props} />} />
+    <Route path='/login' exact render={props => <Login {...props} />} />
     <Route path='/users/activate/:token' exact render={props => <Activate {...props} />} />
-    
+    <Route path='/users/password/forget' exact render={props => <ForgetPassword {...props} />} />
     <Redirect to='/' />
   </Switch>
 </BrowserRouter>,
