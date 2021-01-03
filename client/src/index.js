26b42296654a36ccd4f978a17fbd9f4443fe83ae
prateekchandra <prateekchandra1027@gmail.com>
2021-01-03 22:47:20 +0530

    Reset passowrd is created according to front end i.e ResetPassword.jsx and in index.js Resetpassword is created

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import Activate from './screens/Activate.jsx';
 import Login from './screens/Login.jsx';
 import 'react-toastify/dist/ReactToastify.css';
 import ForgetPassword from './screens/ForgetPassword.jsx';
+import ResetPassword from './screens/ResetPassword.jsx';
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -17,6 +19,7 @@ ReactDOM.render(
     <Route path='/login' exact render={props => <Login {...props} />} />
     <Route path='/users/activate/:token' exact render={props => <Activate {...props} />} />
     <Route path='/users/password/forget' exact render={props => <ForgetPassword {...props} />} />
+    <Route path='/users/password/reset/:token' exact render={props => <ResetPassword {...props} />} />
     <Redirect to='/' />
   </Switch>
 </BrowserRouter>,
